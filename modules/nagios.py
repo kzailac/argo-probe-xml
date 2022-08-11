55ae7426dd53f5eccdad320d5ddecb7fad259bad
Katarina Zailac <katarina.zailac@gmail.com>
2022-08-11 15:44:46 +0200

    Improved Nagios to handle multiline messages

--- modules/nagios.py
+++ modules/nagios.py
@@ -6,26 +6,45 @@ class Nagios:
 
     def __init__(self):
         self._code = self.OK
-        self._msg = "OK"
+        self._msgs = []
+        self._final_msg = ""
+        self.statuses = ["OK", "WARNING", "CRITICAL", "UNKNOWN"]
 
     def ok(self, msg):
-        self._msg = f"OK - {msg}"
-        self._code = self.OK
+        self._msgs.append(msg)
 
     def warning(self, msg):
-        self._msg = f"WARNING - {msg}"
-        self._code = self.WARNING
+        self._msgs.append(msg)
+        if self._code not in [self.CRITICAL, self.WARNING]:
+            self._code = self.WARNING
 
     def critical(self, msg):
-        self._msg = f"CRITICAL - {msg}"
-        self._code = self.CRITICAL
+        self._msgs.append(msg)
+        if self._code != self.UNKNOWN:
+            self._code = self.CRITICAL
 
     def unknown(self, msg):
-        self._msg = f"UNKNOWN - {msg}"
+        self._msgs.append(msg)
         self._code = self.UNKNOWN
+
+    def set_final_msg(self, msg):
+        self._final_msg = msg
 
     def get_code(self):
         return self._code
 
     def get_msg(self):
-        return self._msg
+        if self._final_msg:
+            final_msg = f"{self.statuses[self._code]} - {self._final_msg}"
+
+        elif len(self._msgs) == 1 and not self._final_msg:
+            final_msg = f"{self.statuses[self._code]} - {self._msgs[0]}"
+
+        else:
+            final_msg = self.statuses[self._code]
+
+        if len(self._msgs) != 1:
+            for msg in self._msgs:
+                final_msg = f"{final_msg}\n{msg}"
+
+        return final_msg
